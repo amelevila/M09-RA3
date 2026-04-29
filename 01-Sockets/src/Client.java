4a4c9a4fcf8a2f1beabf5553497f2456d7f5247b
amelevila <amelevila78@gmail.com>
2026-04-29 19:54:26 +0200

    01-Sockets fet a classe

--- 01-Sockets/src/Client.java
+++ 01-Sockets/src/Client.java
@@ -1,3 +1,43 @@
-public class Client {
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
+public class Client {
+    Servidor servidor = new Servidor();
+    
+    private final int PORT = servidor.PORT;
+    private final String HOST = servidor.HOST;
+
+    private Socket socket;
+    private PrintWriter out;
+
+    private void connecta() throws UnknownHostException, IOException {
+        socket = new Socket(HOST, PORT);
+        out = new PrintWriter(socket.getOutputStream(), true);
+        System.out.printf("Connectat a servidor en %s:%d%n", HOST, PORT);
+    }
+
+    private void tanca() throws IOException {
+        socket.close();
+        out.close();
+        System.out.println("Client tancat");
+    }
+
+    private void envia(String msg) {
+        out.println(msg);
+        System.out.println("Enviat al servidor: " + msg);
+    }
+
+    public static void main(String[] args) throws UnknownHostException, IOException {
+        Client client = new Client();
+        client.connecta();
+        client.envia("Prova d'enviament 1");
+        client.envia("Prova d'enviament 2");
+        client.envia("Adeu!");
+
+        System.out.println("Prem Enter per tancar el client...");
+        System.in.read();
+        client.tanca();
+    }
 }
--- 01-Sockets/src/Servidor.java
+++ 01-Sockets/src/Servidor.java
@@ -1,3 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Servidor {
-    
+    public final int PORT = 7777;
+    public final String HOST = "localhost";
+
+    private ServerSocket srvSocket;
+    private Socket clientSocket;
+
+    private void connecta() throws IOException {
+        System.out.printf("Servidor en marxa a %s:%d%n", HOST, PORT);
+        srvSocket = new ServerSocket(PORT);
+        System.out.printf("Esperant connexions a %s:%d%n", HOST, PORT);
+        clientSocket = srvSocket.accept();
+        System.out.println("Client connectat: /127.0.0.1");
+    }
+
+    private void repDades() throws IOException {
+        BufferedReader entrada = new BufferedReader(
+            new InputStreamReader(clientSocket.getInputStream())
+        );
+
+        String linia;
+        while ((linia = entrada.readLine()) != null) {
+            System.out.println("Rebut: " + linia);
+        }
+
+        entrada.close();
+    }
+
+    private void tanca() throws IOException {
+        srvSocket.close();
+        clientSocket.close();
+        System.out.println("Servidor tancat.");
+    } 
+
+    public static void main(String[] args) throws IOException {
+        Servidor servidor = new Servidor();
+        servidor.connecta();
+        servidor.repDades();
+        servidor.tanca();
+    }
 }
